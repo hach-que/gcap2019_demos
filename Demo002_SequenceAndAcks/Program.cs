7bb3c21ea9ccf286e6ff6aee69dd823f7be4b91b
June Rhodes <june@networknext.com>
2019-10-01 14:07:54 +1000

    Add demo for sequences / acks

--- Demo002_SequenceAndAcks/Program.cs
+++ Demo002_SequenceAndAcks/Program.cs
@@ -6,7 +6,7 @@ using System.Net.Sockets;
 using System.Text;
 using System.Threading;
 
-namespace Demo001_Connections
+namespace Demo002_Connections
 {
     /*
     
@@ -15,11 +15,15 @@ namespace Demo001_Connections
     
     */
 
-    delegate void ReceivePacket(IPEndPoint fromAddr, byte[] message);
+    delegate void MessageReceived(IPEndPoint fromAddr, byte[] message);
 
     delegate void ClientDisconnected(IPEndPoint addr);
 
     delegate void ClientConnected(IPEndPoint addr);
+
+    delegate void MessageAcked(IPEndPoint sentToAddr, byte[] message);
+
+    delegate void MessageLost(IPEndPoint sentToAddr, byte[] message);
 
     class GlobalConfig
     {
@@ -33,21 +37,36 @@ namespace Demo001_Connections
         private readonly UdpClient _udp;
         private readonly IPEndPoint _target;
         private readonly Queue<byte[]> _sendQueue;
-        private readonly ReceivePacket _onReceive;
+        private readonly MessageReceived _onReceive;
+        private readonly MessageAcked _onAck;
+        private readonly MessageLost _onLost;
         private readonly ClientDisconnected _onDisconnect;
-        private int _sendQueueSize = 0;
+        private int _sendQueueSize;
         private int _timeoutAccumulator;
         private bool _connected;
+        private ushort _sendSeq;
+        private ushort _ackSeq;
+        private uint _ackBitField;
+        private Dictionary<ushort, byte[][]> _pendingSendAcks;
+        private const int PACKET_HEADER_SIZE_BYTES = sizeof(ushort) * 2 + sizeof(uint);
+        private const int MESSAGE_HEADER_SIZE_BYTES = sizeof(ushort);
 
-        public NetworkConnection(UdpClient udp, IPEndPoint target, ReceivePacket onReceive, ClientDisconnected onDisconnect)
+        public NetworkConnection(UdpClient udp, IPEndPoint target, MessageReceived onReceive, ClientDisconnected onDisconnect, MessageAcked onAck, MessageLost onLost)
         {
             _udp = udp;
             _target = target;
             _sendQueue = new Queue<byte[]>();
             _onReceive = onReceive;
+            _onAck = onAck;
+            _onLost = onLost;
             _onDisconnect = onDisconnect;
             _connected = true;
             _timeoutAccumulator = 0;
+            _sendSeq = 0;
+            _ackSeq = 0;
+            _ackBitField = 0;
+            _sendQueueSize = PACKET_HEADER_SIZE_BYTES;
+            _pendingSendAcks = new Dictionary<ushort, byte[][]>();
         }
 
         public void EnqueueReceive(IPEndPoint fromAddr, byte[] packet)
@@ -56,8 +75,77 @@ namespace Demo001_Connections
             _timeoutAccumulator = 0;
             _connected = true;
 
+            // Read the sequence, ack and ack bitfield. In this case, the sequence
+            // is the remote connection's sequence - and thus the sequence number
+            // we will be acknowledging. Meanwhile the acks are for the messages
+            // that we sent out.
+            var remoteSeq = BitConverter.ToUInt16(packet, 0);
+            var remoteAck = BitConverter.ToUInt16(packet, sizeof(ushort));
+            var remoteAckBitfield = BitConverter.ToUInt32(packet, sizeof(ushort) * 2);
+
+            /*******
+             * 
+             * WARNING! This code does not handle sequence number overflow. Refer to
+             * https://web.archive.org/web/20181107181433/https://gafferongames.com/post/reliability_ordering_and_congestion_avoidance_over_udp/#handling-sequence-number-wrap-around
+             * for information on how to handle this.
+             * 
+             ********/
+
+            // Update our acknowledgement of packets the remote machine sent.
+            var lastGot = _ackSeq;
+            var nowGot = remoteSeq;
+            if (nowGot == lastGot)
+            {
+                // Remote acknowledgement is in the same state, nothing to do.
+            }
+            else
+            {
+                // We need to move the acknowledgement of the packet we last got
+                // into the bitfield first.
+                _ackBitField <<= 1;
+                _ackBitField |= 0x1;
+
+                // Shift bitfield across for all the packets we lost in between.
+                if (nowGot - lastGot > 1)
+                {
+                    _ackBitField <<= (nowGot - lastGot) - 1;
+                }
+            }
+            _ackSeq = remoteSeq;
+
+            // Read the acknowledgement data from the remote machine about packets we sent.
+            if (_pendingSendAcks.ContainsKey(remoteAck))
+            {
+                // If it's in the ack field, the remote machine received it.
+                foreach (var message in _pendingSendAcks[remoteAck])
+                {
+                    _onAck(_target, message);
+                }
+                _pendingSendAcks.Remove(remoteAck);
+            }
+            for (var i = 0; i < sizeof(uint); i++)
+            {
+                var seqInBitfield = (ushort)(remoteAck - i - 1);
+                var isAcked = ((remoteAckBitfield >> i) & 0x1) == 0x1;
+                if (_pendingSendAcks.ContainsKey(seqInBitfield))
+                {
+                    foreach (var message in _pendingSendAcks[seqInBitfield])
+                    {
+                        if (isAcked)
+                        {
+                            _onAck(_target, message);
+                        }
+                        else
+                        {
+                            _onLost(_target, message);
+                        }
+                    }
+                }
+                _pendingSendAcks.Remove(seqInBitfield);
+            }
+
             // Take the raw packet and split it out into the messages it contains.
-            var offset = 0;
+            var offset = PACKET_HEADER_SIZE_BYTES;
             while (offset < packet.Length)
             {
                 var len = BitConverter.ToUInt16(packet, offset);
@@ -73,7 +161,7 @@ namespace Demo001_Connections
 
         public void EnqueueSend(byte[] message)
         {
-            var dataLength = 2 /* uint16 length */ + message.Length;
+            var dataLength = MESSAGE_HEADER_SIZE_BYTES + message.Length;
 
             if (dataLength > GlobalConfig.MAX_MTU)
             {
@@ -99,19 +187,34 @@ namespace Demo001_Connections
         {
             // Create buffer for outgoing packet.
             var buffer = new byte[GlobalConfig.MAX_MTU];
-            var offset = 0;
+
+            // Write sequence, ack and ack bitfield.
+            var sendSeqBytes = BitConverter.GetBytes(_sendSeq);
+            var ackSeqBytes = BitConverter.GetBytes(_ackSeq);
+            var ackBitfieldBytes = BitConverter.GetBytes(_ackBitField);
+            Array.Copy(sendSeqBytes, 0, buffer, 0, sizeof(ushort));
+            Array.Copy(ackSeqBytes, 0, buffer, sizeof(ushort), sizeof(ushort));
+            Array.Copy(ackBitfieldBytes, 0, buffer, sizeof(ushort) * 2, sizeof(uint));
+
+            // Write message data.
+            var offset = PACKET_HEADER_SIZE_BYTES;
             foreach (var item in _sendQueue)
             {
-                var lenBytes = BitConverter.GetBytes((UInt16)item.Length);
-                Array.Copy(lenBytes, 0, buffer, offset, 2);
-                Array.Copy(item, 0, buffer, offset + 2, item.Length);
+                var lenBytes = BitConverter.GetBytes((ushort)item.Length);
+                Array.Copy(lenBytes, 0, buffer, offset, sizeof(ushort));
+                Array.Copy(item, 0, buffer, offset + MESSAGE_HEADER_SIZE_BYTES, item.Length);
                 offset += 2 + item.Length;
             }
+
+            // Add messages to "pending acks" - messages we've sent but are waiting
+            // to see if the remote machine acks them or not.
+            _pendingSendAcks.Add(_sendSeq, _sendQueue.ToArray());
 
             // Send packet.
             _udp.Send(buffer, offset, _target);
             _sendQueue.Clear();
-            _sendQueueSize = 0;
+            _sendQueueSize = PACKET_HEADER_SIZE_BYTES;
+            _sendSeq++;
         }
 
         // Call Update at a regular interval.
@@ -139,11 +242,11 @@ namespace Demo001_Connections
         private readonly IPEndPoint _target;
         private readonly NetworkConnection _server;
 
-        public GameClient(string address, int port, ReceivePacket onReceive, ClientDisconnected onTimeout)
+        public GameClient(string address, int port, MessageReceived onReceive, ClientDisconnected onTimeout, MessageAcked onAck, MessageLost onLost)
         {
             _udp = new UdpClient(0) { Client = { Blocking = false } };
             _target = new IPEndPoint(IPAddress.Parse(address), port);
-            _server = new NetworkConnection(_udp, _target, onReceive, onTimeout);
+            _server = new NetworkConnection(_udp, _target, onReceive, onTimeout, onAck, onLost);
         }
 
         public void Send(byte[] message)
@@ -179,17 +282,21 @@ namespace Demo001_Connections
     {
         private readonly UdpClient _udp;
         private readonly Dictionary<IPEndPoint, NetworkConnection> _clients;
-        private readonly ReceivePacket _onReceive;
+        private readonly MessageReceived _onReceive;
         private readonly ClientConnected _onClientConnected;
         private readonly ClientDisconnected _onClientDisconnected;
+        private readonly MessageAcked _onAck;
+        private readonly MessageLost _onLost;
 
-        public GameServer(int port, ReceivePacket onReceive, ClientConnected onClientConnected, ClientDisconnected onClientDisconnected)
+        public GameServer(int port, MessageReceived onReceive, ClientConnected onClientConnected, ClientDisconnected onClientDisconnected, MessageAcked onAck, MessageLost onLost)
         {
             _udp = new UdpClient(port) { Client = { Blocking = false } };
             _clients = new Dictionary<IPEndPoint, NetworkConnection>();
             _onReceive = onReceive;
             _onClientConnected = onClientConnected;
             _onClientDisconnected = onClientDisconnected;
+            _onAck = onAck;
+            _onLost = onLost;
         }
 
         public void Send(IPEndPoint clientAddr, byte[] message)
@@ -202,7 +309,7 @@ namespace Demo001_Connections
                 _clients[clientAddr] = new NetworkConnection(_udp, clientAddr, _onReceive, (fromAddr) => {
                     _clients.Remove(fromAddr);
                     _onClientDisconnected(fromAddr);
-                });
+                }, _onAck, _onLost);
                 _onClientConnected(clientAddr);
             }
 
@@ -227,7 +334,7 @@ namespace Demo001_Connections
                     _clients[receiveAddr] = new NetworkConnection(_udp, receiveAddr, _onReceive, (fromAddr) => {
                         _clients.Remove(fromAddr);
                         _onClientDisconnected(fromAddr);
-                    });
+                    }, _onAck, _onLost);
                     _onClientConnected(receiveAddr);
                 }
 
@@ -258,6 +365,14 @@ namespace Demo001_Connections
                     Console.WriteLine("[server] Client connected - " + addr);
                 }, (addr) => {
                     Console.WriteLine("[server] Client disconnected - " + addr);
+                }, (sentToAddr, message) =>
+                {
+                    // On ack.
+                }, (sentToAddr, message) =>
+                {
+                    // This makes it harder to read the output for the purposes of the demo, but you can uncomment
+                    // it if you want to see when data sent back to the client is lost.
+                    // Console.WriteLine("[server] Message got lost in transit back to client: " + Encoding.ASCII.GetString(message));
                 });
 
                 Console.WriteLine("[server] Started");
@@ -282,6 +397,12 @@ namespace Demo001_Connections
                 }, (addr) => {
                     Console.WriteLine("[client] Lost connection to server");
                     running = false;
+                }, (sentToAddr, message) =>
+                {
+                    // On ack.
+                }, (sentToAddr, message) =>
+                {
+                    Console.WriteLine("[client] Message got lost in transit to server: " + Encoding.ASCII.GetString(message));
                 });
 
                 Console.WriteLine("[client] Started");
